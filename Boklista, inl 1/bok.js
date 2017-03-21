11a9b522252efc6759980b485f33477b2fde8c8c
nauberinho <nauber91@hotmail.com>
2017-03-21 21:56:36 +0100

    Add files via upload

--- Boklista, inl 1/bok.js
+++ Boklista, inl 1/bok.js
@@ -1,24 +1,17 @@
-
+let body = document.getElementsByTagName('body')[0];
 let inputtitle = document.getElementById('inputtitle')
 let inputauthor = document.getElementById('inputauthor');
-
 let addbtn = document.getElementById('addbook');
-
 let searchbtn = document.getElementById('searchbooks')
-
 let boklista = document.getElementById('lista');
-
 let key = 'PRu3m';
-
 let addfromApi = document.getElementById('addfromAPI');
 
-function removeChildren(parent){
-    
+function removeChildren(parent) {
     while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
+        parent.removeChild(parent.firstChild);
+    };
 };
-};
-    
 //------------------------------GET API KEY--------------------------------------
 /*function getKey(){
 
@@ -46,160 +39,119 @@ function removeChildren(parent){
     req.send();
 }
 */
-
-
-
-
 //---------------------------------------------------------------------------------
-
 //-------------------FUNCTION INSERT BOOKS-----------------------------------------
-
-function insertBooks(inputtitle1, inputauthor1){
-    
+function insertBooks(inputtitle1, inputauthor1) {
     console.log(inputtitle1 + ',' + inputauthor1)
     let booksurl = "https://www.forverkliga.se/JavaScript/api/crud.php?key=" + key + '&op=insert&title=nauber&title=' + inputtitle1 + "&author=" + inputauthor1;
-
-    
     let bookreq = new XMLHttpRequest;
-    
-    bookreq.onreadystatechange = function(event){
-        
-        if(bookreq.status===200 && bookreq.readyState===4){
-            
-            
+    bookreq.onreadystatechange = function (event) {
+        if (bookreq.status === 200 && bookreq.readyState === 4) {
             let myResponseParsed = JSON.parse(bookreq.responseText);
-            
-            if(myResponseParsed.status!=='error'){
-                
+            if (myResponseParsed.status !== 'error') {
                 console.log(myResponseParsed.status)
-               
                 getBooks()
             }
-            
-            else if(myResponseParsed.status=='error'){
-                
+            else if (myResponseParsed.status == 'error') {
                 console.log(myResponseParsed.status)
                 insertBooks(inputtitle.value, inputauthor.value);
-                
-                
             }
-  
-            
         }
-        
     }
-    
     bookreq.open('GET', booksurl);
     bookreq.send();
-    
-    
 }
-
 //--------------------------------FUNCTION GET BOOKS-------------------------------------------
-
 let arrayEditableTitle = [];
 let arrayEditableAuthor = [];
 let arrayId = [];
 
-function getBooks(){
-    
+function getBooks() {
     removeChildren(boklista);
-
     let booksurl = "https://www.forverkliga.se/JavaScript/api/crud.php?key=" + key + "&op=select"; //+ searchbox.value;
-    
     let bookreq = new XMLHttpRequest;
-    
-    bookreq.onreadystatechange = function(event){
-        
-        if(bookreq.readyState===4 && bookreq.readyState===4){
-            
-            
+    bookreq.onreadystatechange = function (event) {
+        if (bookreq.readyState === 4 && bookreq.readyState === 4) {
             let myResponseParsed = JSON.parse(bookreq.responseText);
-            
-            if(myResponseParsed.status!=='error'){
-                
-                if(myResponseParsed.data.author==undefined){
-                for(i=0;i<myResponseParsed.data.length;i++){
-                
-                let li = document.createElement('LI');
-                li.className = 'books-li';
-                let titleAndAuthor = document.createElement('DIV');
-                titleAndAuthor.innerHTML = myResponseParsed.data[i].title + ', ' + myResponseParsed.data[i].author;
-                titleAndAuthor.id = myResponseParsed.data[i].id;
-                titleAndAuthor.className = 'titleAndAuthor';
-                li.appendChild(titleAndAuthor);
-                let remove = document.createElement('I');
-                remove.innerHTML = 'delete_forever';
-                remove.className='material-icons';
-                remove.id=myResponseParsed.data[i].id;
-                li.appendChild(remove)
-                remove.addEventListener('click', deleteBooks)
-                titleAndAuthor.addEventListener('click', editBooks);
-                boklista.appendChild(li);
-        
+            if (myResponseParsed.status === 'success') {
+                if (myResponseParsed.data !== undefined) {
+                    for (i = 0; i < myResponseParsed.data.length; i++) {
+                        let id = myResponseParsed.data[i].id;
+                        //console.log('New id: ' + id + ', data: ' + myResponseParsed.data + ', obj: ' + myResponseParsed);
+                        let li = document.createElement('LI');
+                        li.className = 'books-li';
+                        let titleAndAuthor = document.createElement('DIV');
+                        titleAndAuthor.innerHTML = myResponseParsed.data[i].title + ', ' + myResponseParsed.data[i].author;
+                        titleAndAuthor.id = id;
+                        titleAndAuthor.className = 'titleAndAuthor';
+                        li.appendChild(titleAndAuthor);
+                        let remove = document.createElement('I');
+                        remove.innerHTML = 'delete_forever';
+                        remove.className = 'material-icons';
+                        remove.id = id;
+                        //console.log('alltid ok? '+myResponseParsed.data[i].id)
+                        li.appendChild(remove);
+                        remove.addEventListener('click', function(event) {
+                            if( !event ) {
+                                console.log('event is undefined!');
+                            } else 
+                                deleteBooks(event);
+                        });
+                        titleAndAuthor.addEventListener('click', editBooks);
+                        boklista.appendChild(li);
+                    }
+                    console.log(myResponseParsed);
+                } else {
+                    console.log('serious errorrr')
                 }
-              
-                console.log(myResponseParsed);
-                    
-                }
-                
             }
-            
-            else if(myResponseParsed.status=='error'){
-                
+            else if (myResponseParsed.status == 'error') {
                 getBooks();
-                
             }
-            
         }
-        
     }
-    
     bookreq.open('GET', booksurl);
     bookreq.send();
-    
-    
-}
+};
 
-addbtn.addEventListener('click', function(){
 
+addbtn.addEventListener('click', function () {
     insertBooks(inputtitle.value, inputauthor.value);
 })
-
-
-searchbtn.addEventListener('click', function(){
-
+searchbtn.addEventListener('click', function () {
     getBooks();
-})
-
+});
 addfromApi.addEventListener('click', addFromOtherApi);
 
-
-function addFromOtherApi(){
-    
+function addFromOtherApi() {
     let bookValue = document.getElementById('inputotherAPI').value;
     let booksurl = 'https://www.googleapis.com/books/v1/volumes?q=' + bookValue + '&origin=*';
-    
     let bookreq = new XMLHttpRequest();
-    
-    bookreq.onreadystatechange = function(event){
         
-        if(bookreq.readyState===4 && bookreq.readyState===4){
+
+    bookreq.onreadystatechange = function (event) {
+        
+        if (bookreq.readyState === 4 && bookreq.status === 200) {
+            let response = bookreq.responseText;
+            let myResponseParsed = JSON.parse(response);
+            console.log(myResponseParsed.items)
             
-            console.log(bookreq.responseText);
-            let myResponseParsed = JSON.parse(bookreq.responseText);
-  
+            for(var obj in myResponseParsed.items){
+                console.log(myResponseParsed.items[obj].volumeInfo.authors, myResponseParsed.items[obj].volumeInfo.title);
+            insertBooks(myResponseParsed.items[obj].volumeInfo.title, myResponseParsed.items[obj].volumeInfo.authors)
+            
+            console.log('myResponseParsed: ' + myResponseParsed)
         }
-        
     }
-    
-    bookreq.open('GET', booksurl, true);
-    bookreq.send(); 
-    
+        
+  
 }
+      bookreq.open('GET', booksurl, true);    
+    bookreq.send();
+    
+};
 
-
-function editBooks(event){
+function editBooks(event) {
     let newInput = document.createElement('input');
     newInput.className = 'new-input';
     console.log(event.target);
@@ -208,9 +160,7 @@ function editBooks(event){
     newInput.value = editText;
     event.target.appendChild(newInput);
     newInput.focus();
-    
-    
-newInput.addEventListener('blur', updateEdit);
+    newInput.addEventListener('blur', updateEdit);
 
     function updateEdit() {
         let thisElement = event.target;
@@ -228,71 +178,72 @@ newInput.addEventListener('blur', updateEdit);
                 updateParse = JSON.parse(req.responseText)
                 console.log(updateParse);
                 if (updateParse.status != 'error') {
-                    
-                    console.log('Inne i edit med utan fel');
-                    
+                    console.log('Inne i edit men utan fel');
                 }
                 else {
-                    
-                    console.log('Inne i edit med med felmeddelande');
+                    console.log('Inne i edit med felmeddelande');
                     updateEdit();
-                    
                 }
-                
                 getBooks();
             }
         };
-        
-    req.send();
-        
-        
-        
-        
-        
-        
+        req.send();
     };
 }
 
-
 let deleteButton = document.getElementsByClassName('remove')[0];
 
-
-
-function deleteBooks() {
-    let thisIdToRemove = event.target.id;
+function deleteBooks(deleteevent) {
+    let thisIdToRemove = deleteevent.target.id;
     let req = new XMLHttpRequest;
-    req.open('GET', `https://www.forverkliga.se/JavaScript/api/crud.php?key=${key}&op=delete&id=${thisIdToRemove}`);
+    let url = `https://www.forverkliga.se/JavaScript/api/crud.php?key=${key}&op=delete&id=${thisIdToRemove}`;
+    req.open('GET', url);
     req.onreadystatechange = function (e) {
         if (req.readyState === 4 && req.status === 200) {
             jsonTextParse = JSON.parse(req.responseText);
-            
-            
-            if(jsonTextParse.status !== 'error'){
-                console.log(jsonTextParse);
-                console.log('Borta!');
+            if (jsonTextParse.status !== 'error' && thisIdToRemove!==undefined) {
+                
+
                 getBooks();
             }
-            
             else {
-                deleteBooks();
+                console.log('Detta är error: ' + jsonTextParse.status + ', url= '+url);
+                deleteBooks(deleteevent);
             }
-        
-     
-            }
+        }
     }
     req.send();
 }
+let divoveraddfromother = document.getElementById('divoveraddfromother');
+let divoveraddbook = document.getElementById('divoveraddbook');
+let divoveraddfromotherclicked = false;
+let divoveraddbookclicked = false;
 
-
-
-let divOveraddbookdiv = document.getElementById('divoveraddbook');
+let addfromotherdiv = document.getElementsByClassName('addfromotherdiv')[0];
 let addbookdiv = document.getElementsByClassName('addbookdiv')[0];
 
-let divOveraddbookfromother = document.getElementById('divoveraddfromother');
-let addbookfromotherdiv = document.getElementsByClassName('addfromotherdiv')[0]
+divoveraddbook.addEventListener('click', function(event){
+
+    scrollUpDivs(event.target)
+});
+
+divoveraddfromother.addEventListener('click', function(event){
+    console.log('divoveraddfromotherclicked:')
+    scrollUpDivs(event.target);
+});
+
+function scrollDownDivs(e){
+
+        
+         e.style.height='100%';
 
 
+};
 
 
-//getKey()
+function scrollUpDivs(e){
+         e.style.height='0%';
+};
 
+
+    //getKey()
