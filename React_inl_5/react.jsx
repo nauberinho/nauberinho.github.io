a82646eeb1a52be2317d8381bf9fb1eac7c2ddee
nauberinho <nauber91@hotmail.com>
2017-05-07 11:18:17 +0200

    Update react.jsx

--- React_inl_5/react.jsx
+++ React_inl_5/react.jsx
@@ -88,7 +88,8 @@ class ChangeState extends React.Component {
         button1class: props.button1class,
         button2class: props.button2class,
         button3class: 'not-active',
-        text: props.text
+        text: props.text,
+        clicks: [0,0,0]
     };
     this.changeClass = this.changeClass.bind(this);
 
@@ -97,26 +98,38 @@ class ChangeState extends React.Component {
     changeClass(event){
         
         console.log(event.target.class)
-        
+        let newclicksList = this.state.clicks;
         if(event.target.id=='button1'){
+            newclicksList[event.target[data-index]]++
             this.setState({button1class: 'active'})
             this.setState({button2class: 'not-active'})
+            this.setState({button3class: 'not-active'}))
+            if(Math.max(newclicksList) == newclicksList[event.target[data-index]]){                                  
+                this.setState({button1class: 'most-clicked'})
+            }
             this.setState({text: 'Button 1 är aktiv'})
-            
-            
         }
         
         else if(event.target.id=='button2'){
+            newclicksList[event.target[data-index]]++
             this.setState({button1class: 'not-active'})
             this.setState({button2class: 'active'})
+            this.setState({button3class: 'not-active'})
+            if(Math.max(newclicksList) == newclicksList[event.target[data-index]]){                                  
+                this.setState({button2class: 'most-clicked'})
+            }
             this.setState({text: 'Button 2 är aktiv'})
-            
         }
         
         else if(event.target.id=='button3'){
+            newclicksList[event.target[data-index]]++
+
             this.setState({button1class: 'not-active'})
             this.setState({button2class: 'not-active'})
             this.setState({button3class: 'active'})
+            if(Math.max(newclicksList) == newclicksList[event.target[data-index]]){                                  
+                this.setState({button3class: 'most-clicked'})
+            }
             this.setState({text: 'Button 3 är aktiv'})
         };
         
@@ -127,9 +140,9 @@ class ChangeState extends React.Component {
         render() {
             return (
               <div className='section-three'>
-                <button className={this.state.button1class} id='button1' onClick={this.changeClass}></button>
-                <button id='button2' onClick={this.changeClass} className={this.state.button2class}></button>
-                <button id='button3' onClick={this.changeClass} className={this.state.button3class}></button>
+                <button data-index={0} className={this.state.button1class} id='button1' onClick={this.changeClass}></button>
+                <button data-index={1} id='button2' onClick={this.changeClass} className={this.state.button2class}></button>
+                <button data-index={2} id='button3' onClick={this.changeClass} className={this.state.button3class}></button>
                 <div>Text: {this.state.text}</div>
               </div>
             );
